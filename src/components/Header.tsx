aa2acb63c3d7e0b21cc78aed3cd25e717e8bbf61
Timóteo Stifft <timoteostifft@gmail.com>
2022-11-22 18:37:13 -0300

    feat: header

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+import logo from '../assets/logo.png'
+
+function Header () {
+  return (
+    <header className="h-20 bg-white_100 shadow-xl">
+      <div className="h-full max-w-5xl px-5 mx-auto flex flex-row items-center">
+        <img src={logo} alt="" className="h-20 w-20"/>
+        <span className='font-poppins font-semibold text-title text-3xl'>Users API</span>
+        <nav className='ml-6'>
+          <a href='' className='font-poppins text-title text-lg'>Home</a>
+          <a href='https://randomuser.me/' className='font-poppins text-title text-lg ml-4'>Docs</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
+
+export default Header
+
+
+{/*  */}
+        // <span>User List</span>
+        // <div className=""></div>
--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom"
+import Header from "../../components/Header"
 
 function defaultLayout (){
   return (
-    <div className="box-border h-screen flex flex-col mx-auto my-0 px-5 max-w-6xl w-full">
-      <Outlet />
+    <div className="h-max w-full bg-background">
+      <Header />
+      <div className="flex flex-col max-w-5xl mx-auto mt-16 px-5">
+        <Outlet />
+      </div>
     </div>
   )
 }
