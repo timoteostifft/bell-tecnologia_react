c553c1d7bc433c6928c174987c970de63f22bef6
Timóteo Stifft <timoteostifft@gmail.com>
2022-11-22 18:39:04 -0300

    feat: default theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      white_100: '#FFFFFF',
+      blue_400: '#4772D7',
+
+      background: '#FAFAFA',
+      title: '#314149',
+      text: '#7895A5',
+      strong: '#628293',
+    },
+    fontFamily: {
+      poppins: "'Poppins', sans-serif;"
+    },
   },
   plugins: [],
 }
