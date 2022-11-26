792a22d1974ee81eaabb22a30a5843c5cfc60399
Timóteo Stifft <timoteostifft@gmail.com>
2022-11-26 06:34:36 -0300

    feat: info component functionality

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -1,9 +1,30 @@
 import { User } from 'phosphor-react'
+import { useContext, useEffect } from 'react'
+import { UsersContext } from '../contexts/UsersContext'
 
-function Info() {
+import { NavLink } from 'react-router-dom';
+
+interface Props {
+  id: string
+}
+
+function Info({ id }: Props) {
+
+  const { updateSelectedUserId } = useContext(UsersContext)
+
+  function handleUpdateSelectedUserId() {
+    updateSelectedUserId(id)
+  }
+
   return (
-    <button className="my-0 ml-auto border-blue_400 flex items-center content-center" title='Go to Profile'>
-      <User size={22} weight="regular" color="#4772d7" className='p-1.5 box-content'/>
+    <button 
+      className="my-0 ml-auto border-blue_400 flex items-center content-center" 
+      title='Go to Profile'
+      onClick={() => handleUpdateSelectedUserId()}  
+    >
+      <NavLink to={'/profile'}>
+        <User size={22} weight="regular" color="#4772d7" className='p-1.5 box-content'/>
+      </NavLink>
     </button>
   )
 }
